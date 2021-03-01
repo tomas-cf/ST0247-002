144283dc04f14945ab8fbedadcb81e0dda04763c
tomas-cf <68928429+tomas-cf@users.noreply.github.com>
2021-03-01 11:43:51 -0500

    Se agrega el taller06
    Se agrega el taller06 hasta donde alcancé a resolver en clase, me quedó faltando el punto 02

---
+++ talleres/taller06/Taller06.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller06;
+
+/**
+ * Clase en la cual se implementan los metodos del Taller 6
+ * 
+ * @author Mauricio Toro, Andres Paez
+ */
+public class Taller06 {
+
+   	/**
+	* Metodo que dado un entero n y un conjunto de denominciones de dinero
+	* busque la manera optima de dar el cambio
+	* @param n cantidad a devolver
+	* @param denominaciones conjunto de denominaciones de dinero (monedas, billetes)
+	* @return un conjunto de unidades por denominacion
+	*/
+	public static int[] cambioGreedy(int n, int[] denominaciones) {
+		
+	}
+
+	/**
+	* Metodo que recorre todo el grafo con la intencion de buscar un
+	* camino que represente el menor costo pasando por todos los vertices exactamente
+	* una vez y vuelva al nodo inicial
+	* @param g grafo dado 
+	* @return cual es el costo que tiene
+	*/
+	public static int recorrido(Digraph g) {
+	
+	}
+
+}
