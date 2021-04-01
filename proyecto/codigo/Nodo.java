7798b4e309fba8596ec66aa2b966a98a0075c232
tomas-cf <68928429+tomas-cf@users.noreply.github.com>
2021-03-31 23:37:37 -0500

    Se agrega código entrega02

---
+++ proyecto/codigo/Nodo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+/**
+ * Clase que representa nodos cliente y nodos deposito
+ * @author ljpalaciom
+ */
+public class Nodo { 
+    int id;
+    public Nodo(int id) {
+        this.id = id;
+    }
+}
